b846934ef9e08a6d24effc8ab0ad7ec4b52222f7
Antti Kautiainen <antti@kautiainen.com>
2024-12-28 10:37:12 +0200

    Squashed commit of the following:
    commit f55b27f0df24be37bc971b7bce3e8cf0c0e07712
    Author: Antti Kautiainen <antti@kautiainen.com>
    Date:   Sat Dec 28 10:11:18 2024 +0200
    
        fix: invalid import of webpack config.
    
        Changes to be committed:
        	modified:   src/main/javascript/dao.mjs
            - fix: remove unused and invalid import to webpack config in
            dao.mjs.
    
    commit 9e82879c915ee62a0432fa097168eedc62a22642
    Author: Antti Kautiainen <antti@kautiainen.com>
    Date:   Sat Dec 28 09:11:20 2024 +0200
    
        config: webpack development build configuration
        Changes to be committed:
        	new file:   src/main/javascript/webpack.config.dev.js
            - config: webpack configuration for development mode.
    
    commit fe519bd03719010a6ff486129a353fd6d553807d
    Author: Antti Kautiainen <antti@kautiainen.com>
    Date:   Sat Dec 28 09:09:22 2024 +0200
    
        config-fix: webpack output directories.
    
        Changes to be committed:
        	deleted:    src/main/javascript/dist/javascript/webpack.config.dev.js
            - fix: Moved webpack developmetn build configuration to the proper
            location.
        	modified:   src/main/javascript/webpack.config.js
            - fix: The output directory to the proper output directory.
    
    commit 1e6b9633849f25649fb3e5eb59af820ad5c5fd0f
    Author: Antti Kautiainen <antti@kautiainen.com>
    Date:   Sat Dec 28 09:02:25 2024 +0200
    
        config: development mode compile and watch.
         Changes to be committed:
        	modified:   package-lock.json
            - config: lodash dependency.
        	modified:   package.json
            - config: script compile for development mode building of library.
            - config: script watch for development mode watching of files
            automatically compiling the development products on update of files.
    
    commit 59289185899edc46838f22019502a777a40dfe7a
    Author: Antti Kautiainen <antti@kautiainen.com>
    Date:   Sat Dec 28 09:01:26 2024 +0200
    
        config: webpack development config
    
        The webpack configuration for development build.
        Changes to be committed:
        	new file:   src/main/javascript/dist/javascript/webpack.config.dev.js
            - config: Webpack configuration for development mode building with
            devtools.
    
    commit e802e326ce3ef3906e853b2e88472d0817c18727
    Author: Antti Kautiainen <antti@kautiainen.com>
    Date:   Sat Dec 28 09:00:37 2024 +0200
    
        config: added mode to the webpack.config
        Changes to be committed:
        	modified:   src/main/javascript/webpack.config.js
            - config: mode production to get rid of waring.
    
    commit a8cfcdbcb995bc92b81908d5fbdfb7eda0e6376c
    Author: Antti Kautiainen <antti@kautiainen.com>
    Date:   Sat Dec 28 08:54:06 2024 +0200
    
        feat: interface DAO and basic implementations
    
        The interface DAO<ID, TYPE> reprsents the DAO.
    
        Exception calsses:
        - class Exception<CAUSE> for generic exceptions with defined messages.
        - class NoSuchElementException<CAUSE> for missing resource exception.
        - class UnsupportedException<CAUSE> for an unsupported operation.
    
        The DAO methods are reprsented with callbacks:
        - Updater<ID, TYPE> for updating values.
        - Remover<ID, TYPE> for removing values.
        - Supplier<Entry<ID, TYPE>[]> for getting all values.
        - Creator<ID, TYPE> for creating new values.
        - Getter<ID, TYPE> for getting value of ID.
        - Patcher<ID, TYPE> for patching a value.
    
        Changes to be committed:
        	new file:   src/main/javascript/dao.mjs
            - feat: interface DAO<ID, TYPE>
            - feat: class BasicDao<ID, TYPE> implements DAO<ID, TYPE>
                - Basic implementation returning empty list of values, and
                throwing UnsupportedException for all values.
                - feat: method get is implemented through getAll.
            - feat: class FunctionalDao<ID, TYPE> extends BasicDao<ID, TYPE>
            Functional dao implemeenting methods with functions called, if
            the modified value exists for remove, update, and patch.

---
+++ src/main/javascript/dao.mjs
@@ -0,0 +1,432 @@
+
+/**
+ * The module defining the DAO.
+ * @module dao
+ */
+
+/**
+ * An exception is an error with optional cause type.
+ * @template [CAUSE=any] The cause type.
+ */
+export class Exception extends Error {
+
+    /**
+     * Create a new exception with a message and an optional cause.
+     * @param {string} message The message of the exception. 
+     * @param {CAUSE} [cause] The cause of the exception.
+     */
+    constructor(message, cause = undefined) {
+        super(message, { cause });
+        this.name = this.constructor.name;
+    }
+}
+
+/**
+ * A no such element exception indicates the requested
+ * element does not exist.
+ * @template [CAUSE=any] The cause type.
+ */
+export class NoSuchElementException extends Exception {
+
+    /**
+     * Create a new no such element exception.
+     * @param {string} message The message of the exception.
+     * @param {CAUSE} [cause] The cause of the exception. 
+     */
+    constructor(message, cause = undefined) {
+        super(message, cause);
+    }
+}
+
+/**
+ * An unsupported exception indicates the operation was not supported.
+ * @template [CAUSE=any] The cause type.
+ */
+export class UnsupportedException extends Exception {
+
+    /**
+     * Create a new unsupported exception with a message and an optional cause.
+     * @param {string} message The message of the exception.
+     * @param {CAUSE} [cause] The optional cause. 
+     */
+    constructor(message, cause = undefined) {
+        super(message, cause);
+    }
+}
+
+/**
+ * Are two values equals.
+ * @template TYPE The type of the compared values.
+ * @callback Equality
+ * @param {TYPE} compared The compared value.
+ * @param {TYPE} comparee The value compared with.
+ * @returns {boolean} True, if and only if the compared is equal to comparee.
+ */
+
+/**
+ * A predicate testing a value.
+ * @template TYPE The tested type.
+ * @callback Predicate
+ * @param {TYPE} tested The tested value.
+ * @returns {boolean} True, if and only if the tested fulfils the predicate.
+ */
+
+/**
+ * A predicate testing a value of an iteration.
+ * @template TYPE The tested type.
+ * @callback IteratorPredicate
+ * @param {TYPE} tested The tested value.
+ * @param {number} index The index of the iteration.
+ * @returns {boolean} True, if and only if the tested at the index fulfils the predicate.
+ */
+
+/**
+ * A predicate testing a value of an array.
+ * @template TYPE The tested type.
+ * @callback ArrayPredicate
+ * @param {TYPE} tested Teh tested value.
+ * @param {number} index The index of the array.
+ * @param {TYPE[]} array The array containing the value.
+ * @returns {boolean} True, if and only if the tested at the index of the array fulfils the predicate.
+ */
+
+/**
+ * An entry is a identifier-value pair.
+ * @template [ID=string] The identifier type.
+ * @template TYPE The value type.
+ * @typedef {Object} Entry
+ * @property {ID} id The identifier of the entry.
+ * @property {TYPE} value The value of the entry.
+ */
+
+/**
+ * A supplier of a value.
+ * @template TYPE The supplied value type.
+ * @callback Supplier
+ * @returns {Promise<TYPE>} The supplied value.
+ */
+
+/**
+ * A getter of an existing value.
+ * @template [ID=string] The identifier type.
+ * @template TYPE The value type.
+ * @callback Getter
+ * @param {ID} id The queried identifier.
+ * @returns {Promise<TYPE>} The promise of the value associated
+ * with the id.
+ * @throws {NoSuchElementException} The exception rejected, if no
+ * element with given identifier exists.
+ */
+
+/**
+ * Update an existing value.
+ * @template [ID=string] The identifier type.
+ * @template TYPE The updated type.
+ * @callback Updater
+ * @param {ID} id The identifier of the updated value.
+ * @param {TYPE} value The new value associated to the id.
+ * @param {TYPE} [target] The optional target of the update.
+ * @returns {Promise<Void>} The promise of the successful update.
+ * @throws {SyntaxError} The rejected error, if the given value is invalid.
+ * @throws {NoSuchElementException} The rejected exception, if there is no value
+ * associated with the given identifier.
+ * @throws {UnsupportedException} The rejected exception, if the update operation is not supported.
+ */
+
+/**
+ * @template [ID=string] The identifier type.
+ * @template TYPE The updated type.
+ * @callback Patcher
+ * @param {ID} id The identifier of the patched value.
+ * @param {Partial<TYPE>} value The updated properties.
+ * @param {TYPE} [target] The optional target of the patching.
+ * @returns {Promise<Void>} The promise of the successful update.
+ * @throws {SyntaxError} The rejected error, if the given value is invalid.
+ * @throws {NoSuchElementException} The rejected exception, if there is no value
+ * associated with the given identifier.
+ * @throws {UnsupportedException} The rejected exception, if the patch operation is not supported.
+ */
+
+/**
+ * Remove an existing value.
+ * @template [ID=string] The identifeir type.
+ * @callback Remover
+ * @param {ID} id The identifier of the removed value.
+ * @param {TYPE} [target] The optional removed value. 
+ * @returns {Promise<Void>} The promise of the successful removal.
+ * @throws {NoSuchElementException} The rejected exception, if there was no value
+ * associated with the given identifier.
+ * @throws {UnsupportedException} The rejected exception, if the removal operation is not supported.
+ */
+
+/**
+ * Create a new entry with a value. 
+ * @template [ID=string] The identifier type.
+ * @template TYPE The value type of the added value.
+ * @callback Creator
+ * @param {TYPE} value The added value.
+ * @returns {Promise<ID>} The promise of the identifier associated to the created value.
+ * @throws {SyntaxError} The rejected error, if the given value could not be added.
+ * @throws {UnsupportedException} The rejected exception, if the new element creation is not supported.
+ */
+
+/**
+ * A dao reprents a data access object storing and supplying values associated with an 
+ * identifer.
+ * @template [ID=string] The identifier type.
+ * @template TYPE The value type of the DAO.
+ * @typedef {Object} DAO
+ * @property {Getter<ID, TYPE>} get Get the element associated to the identifier.
+ * @property {Supplier<Entry<ID, TYPE>[]>} getAll Get all identifier-value pairs of the dao.
+ * @property {Creator<ID, TYPE>} create Create a new entry in DAO.
+ * @property {Remover<ID>} remove Remove a avleu from DAO.
+ * @property {Updater<ID, TYPE>} update Update an existing value associated to the identifier.
+ * @property {Patcher<ID, TYPE>} patch Update some properties of an existing value associated
+ * to the idnetifier.
+ */
+
+/**
+ * The basic dao options.
+ * @template [ID=string] The identifier type.
+ * @template TYPE The value type of the DAO.
+ * @typedef {Object} BasicDaoOptions
+ * @property {Equality<ID>} [equalId] The identifier equality function.
+ * @property {Equality<VALUE>} [equalValue] The value equality function.
+ */
+
+/**
+ * A basic implementation of a DAO.
+ * @template [ID=string] The identifier type.
+ * @template TYPE The value type of the DAO.
+ * @extends {DAO<ID, TYPE>}
+ */
+export class BasicDao {
+
+    /**
+     * The default equality is a strict equality.
+     * @template VALUE THe type of the tested value.
+     */
+    static DEFAULT_EQUALITY = (/** @type {VALUE} */ compared, /** @type {VALUE} */ comparee) => (
+        compared === comparee
+    );
+
+    /**
+     * Are identifiers equals.
+     * @type {Equality<ID>} 
+     */
+    equalId(compared, comparee) {
+        return this._equalid(compared, comparee);
+    }
+
+    /**
+     * Are values equals.
+     * @type {Equality<TYPE>}
+     */
+    equalValue(compared, comparee) {
+        return this._equalValue(compared, comparee);
+    }
+
+    /**
+     * Create a new basic dao.
+     * @param {BasicDaoOptions<ID, TYPE>} [options] 
+     */
+    constructor(options = {}) {
+        /**
+         * The equaöity of idenfiers.
+         * @type {Equality<ID>}
+         */
+        this._equalId = options.equalId == null ? BasicDao.DEFAULT_EQUALITY : options.equalId;
+        /**
+         * Teh equailty of values.
+         * @type {Equality<TYPE>}
+         */
+        this._equalValue = options.equalValue == null ? BasicDao.DEFAULT_EQUALITY : options.equalValue;
+    }
+
+    getAll() {
+        /** @type {Entry<ID, TYPE>[]} */
+        const result = [];
+        return Promise.resolve(result);
+    }
+
+    /**
+     * Get value of id.
+     * @type {Getter<ID, TYPE>}
+     */
+    get(id) {
+        return this.getAll().then(
+            (values) => {
+                const found = values.find((entry) => (this.equalId(entry.id, id)));
+                if (found == null) {
+                    throw new NoSuchElementException("No such value exists");
+                } else {
+                    return found.value;
+                }
+            }
+        );
+    }
+
+    /**
+     * Update value of identifier.
+     * @type {Updater<ID, TYPE>}
+     */
+    update(id, value, target=undefined) {
+        /**
+         * The promise of a successful update.
+         * @type {Promise<Never>}
+         */
+        const result = Promise.reject(new UnsupportedException("Update not supported"));
+        return result;
+    }
+
+    /**
+     * Patch value associated with id.
+     * @type {Patcher<ID, TYPE>}
+     */
+    patch(id, value, target=undefined) {
+        return Promise.reject(new UnsupportedException("Patch not supported"));
+    }
+
+
+    /**
+     * Remove value associated with identifier.
+     * @type {Remover<ID, TYPE>}
+     */
+    remove(id, target=undefined) {
+        return Promise.reject(new UnsupportedException("Remove not supported"));
+    }
+
+    /**
+     * Create a new value associated with appointed identifier.
+     * @type {Creator<ID, VALUE>}
+     */
+    create(value) {
+    /** @type {Promise<ID>} */     
+    const result = Promise.reject(new UnsupportedException("Create not supported"));
+    return result;
+    }
+}
+
+/**
+ * The options specific to the functional dao. 
+ * @template [ID=string] The identifier type.
+ * @template TYPE The value type of the DAO.
+ * @typedef {Object} FunctionalDaoOptions
+ * @property {Supplier<Entry<ID, TYPE>[]>} [getAll] The function retrieving
+ * all entries from the storage.
+ * @property {Creator<ID, TYPE>} [create] The creator function updating the 
+ * data storage. 
+ * @property {Updater<ID, TYPE>} [update] The updater function updating the 
+ * data storage.
+ * @property {Remover<ID>} [remove] The remover function updating the data storage.
+ * @property {Updater<ID, Partial<TYPE>>} [patch] The partial udpater of the
+ * stored value. 
+ */
+
+/**
+ * A functional DAO is defined by the functions implementing
+ * the DAO operators getAll, create, patch, update, and remove.
+ * @template [ID=string] The identifier type.
+ * @template TYPE The value type of the DAO.
+ * @extends {BasicDao<ID, TYPE>}
+ * @implements {DAO<ID, TYPE>}
+ */
+export class FunctionalDao extends BasicDao {
+
+    /**
+     * Create a new functional DAO.
+     * @param {BasicDaoOptions<ID, TYPE> & FunctionalDaoOptions<ID, TYPE>} [options] The construciton options. 
+     */
+    constructor(options={}) {
+        super(options);
+        /**
+         * @type {Supplier<Entry<ID, TYPE>[]>}
+         */
+        this._getAll = options.getAll;
+        /**
+         * @type {Updater<ID, TYPE>}
+         */
+        this._update = options.update;
+
+        /**
+         * @type {Remover<ID, TYPE>}
+         */
+        this._remove = options.remove;
+
+        /**
+         * @type {Creator<ID, TYPE>}
+         */
+        this._create = options.create;
+
+        /**
+         * @type {Patcher<ID, TYPE>}
+         */
+        this._patch = options.patch;
+    }
+
+    getAll() {
+        if (this._getAll) {
+            return this._getAll();
+        } else {
+            return super.getAll();
+        }
+    }
+
+    update(id, value, target=undefined) {
+        if (this._update) {
+            return this.get(id).then(
+                (target) => {
+                    return this._update(id, value, target);
+                }, 
+                (error) => {
+                    throw new NoSuchElementException("Cannot update non-existing value");
+                }
+            );
+        } else {
+            return super.update(id, value);
+        }
+    }
+
+    remove(id, target = undefined) {
+        if (this._remove) {
+            return this.get(id).then(
+                (target) => {
+                    return this._remove(id);
+                },
+                (error) => {
+                    throw new NoSuchElementException("Cannot remove non-existing value");
+                }
+            )
+        } else {
+            return super.update(id);
+        }
+    }
+    patch(id, value, target = undefined) {
+        if (this._patch) {
+            return this.get(id).then(
+                (target) => {
+                    return this._patch(id, value, target);
+                }, 
+                (error) => {
+                    throw new NoSuchElementException("Cannot patch non-existing value");
+                }
+            )
+            return this._patch(id, value);
+        } else {
+            return super.patch(id, value);
+        }
+    }
+
+    /**
+     * Add a new value ot the DAO.
+     * @param {TYPE} value The added value.
+     * @returns {Promise<ID>} The promise of the identifier associated with the value.
+     */
+    create(value) {
+        if (this._create) {
+            return this._create(value);
+        } else {
+            return super.create(value);
+        }
+    }
+}
--- src/main/javascript/webpack.config.dev.js
+++ src/main/javascript/webpack.config.dev.js
@@ -1,10 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    mode: "production",
+    mode: "development",
     entry: "./src/main/javascript/index.mjs",
+    devtool: 'inline-source-map',
     output: {
-        filename: "daolib.production.js",
+        filename: "daolib.development.js",
         path: path.resolve(__dirname, "..", "..", "dist", "javascript")
     }
 }
